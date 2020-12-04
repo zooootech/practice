6dd0d7c7fc7eae51e6dd07ab64c298cdecb8960d
zooootech <yzmisng@gmail.com>
2020-12-05 01:30:35 +0900

    論理演算子

--- logical_operator.rb
+++ logical_operator.rb
@@ -7,3 +7,16 @@ def parrot_trouble(talking, hour)
     puts "OK"
   end
 end
+
+# ーーーーーーーーーーーーーーーーーーーーーーーーーーー
+
+# その日が遅くまで寝ていられるかどうかを判断するメソッド
+# 休みの場合はTrueを、休みでない場合はFalseを出力
+def sleep_in(is_weekday, is_vacation)
+  # 月〜金曜日ではない場合、または、祝日などの休暇日の場合
+  if is_weekday != true || is_vacation == true
+    puts "True"
+  else
+    puts "False"
+  end
+end
